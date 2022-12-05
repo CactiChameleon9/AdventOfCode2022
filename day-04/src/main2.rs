90cf7bf79dd28f393907e077d18574b41d827b41
CactiChameleon9 <51231053+CactiChameleon9@users.noreply.github.com>
2022-12-05 14:28:46 +0000

    Improve day 4's solution

--- day-04/src/main2.rs
+++ day-04/src/main2.rs
@@ -18,34 +18,19 @@ pub fn main() {
 		let range1str: Vec<&str> = pair.split(',').collect::<Vec<&str>>()[0].split('-').collect();
 		let range1min: usize = range1str[0].parse().unwrap();
 		let range1max: usize = range1str[1].parse().unwrap();
-		let range1 = range1min..range1max + 1;
-		let range1_2 = range1min..range1max + 1; //2nd version because of moved range1
 
 		// Get the string vector of the range, then get the min and max (ints) the get the final range
 		let range2str: Vec<&str> = pair.split(',').collect::<Vec<&str>>()[1].split('-').collect();
 		let range2min: usize = range2str[0].parse().unwrap();
 		let range2max: usize = range2str[1].parse().unwrap();
-		let range2 = range2min..range2max + 1;
 
-		// Check if any values within the range are in the other range (inefficent sadly)
-		let mut overlap = false;
-		for i in range1{
-			if overlap { break ;}
-			
-			if range2.contains(&i) {
-				overlap = true;
-			}
+		// Check if any values within the range are in the other range
+		if (range1max >= range2max && range1min <= range2min) //range2 inside range1
+		|| (range2max >= range1max && range2min <= range1min) //range1 inside range2
+		|| (range1max >= range2max && range1min <= range2max) //range1 greater than range2, but inside
+		|| (range2max >= range1max && range2min <= range1max) { //range2 greater than range1, but inside
+			overlap_pairs += 1;
 		}
-		
-		for i in range2{
-			if overlap { break ;}
-
-			if range1_2.contains(&i) {
-				overlap = true;
-			}
-		}
-
-		if overlap { overlap_pairs += 1; }
 	}
 
 	println!("The number of overlapping pairs are: {overlap_pairs}");
