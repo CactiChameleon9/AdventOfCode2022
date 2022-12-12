e69784882ad9fe808d48080c6c30eda404d6b0f3
CactiChameleon9 <51231053+CactiChameleon9@users.noreply.github.com>
2022-12-12 23:15:52 +0000

    Finish day 9

---
+++ day-09/src/main2.rs
@@ -0,0 +1,95 @@
+use std::fs;
+use std::collections::HashMap;
+
+const ROPE_SIZE: usize = 10;
+
+pub fn main() {
+
+	// Read the file as a string
+	let file: String = fs::read_to_string("input")
+		.expect("Should have been able to read the file");
+
+	let movements: Vec<&str> = file.split('\n').collect();
+
+	/* Rope rules:
+		Tail-head -> LL,RR,UU,DD = Tail += L,R,U,D
+		Tail-head -> LUU,LLU,LDD,LLD,RUU,RRU,RDD,RRD = Tail += LU,LU,LD,LD,RU,RU,RD,RD
+		In words:	if on the same x/y axis move toward
+					if on diffeerent x and y axis, move diagional
+
+	*/
+
+	let mut rope: [[isize; 2]; ROPE_SIZE] = [[0, 0]; ROPE_SIZE];
+
+	let mut visited: HashMap<[isize; 2], usize> = HashMap::new();	
+	visited.insert([0, 0], 1); // visit 0,0
+
+	for movement in movements {
+		if movement.is_empty() { continue; } //skip empty
+		
+		let direction: [isize; 2] = process_direction(movement.split(' ').collect::<Vec<&str>>()[0]);
+		let amount: usize = movement.split(' ').collect::<Vec<&str>>()[1].parse().unwrap();
+
+		for _i in 0..amount {
+		
+			// Move the start
+			rope[0] = vec_add(rope[0], direction);
+
+			for j in 0..rope.len() - 1 {
+			
+				let difference = vec_rem(rope[j + 1], rope[j]);
+
+				// Move the end if the distance is greater than 1 by 1 (root 2)
+				if vec_len(difference) > 1.5 {
+					rope[j + 1] = vec_add(rope[j + 1], vec_trim(difference));
+				}
+			}
+			
+			// Set the vec to visited
+			// println!("{:?}", rope[10]);
+			visited.insert(rope[rope.len() - 1], 1);
+			
+		}
+	}
+
+	let mut visited_total: usize = 0;
+	for (_key, _value) in visited {
+		if _value == 1 {
+			visited_total += 1;
+		}
+	}
+
+	println!("The number of spaces visited by the tail is {visited_total}")	
+	
+}
+
+fn process_direction(direction: &str) -> [isize; 2] {
+	match direction {
+		"U" => [0, 1],
+		"D" => [0, -1],
+		"R" => [1, 0],
+		"L" => [-1, 0],
+		_ => [0, 0]
+	}
+}
+
+
+fn vec_add(vec1: [isize; 2], vec2: [isize; 2]) -> [isize; 2] {
+	[vec1[0] + vec2[0], vec1[1] + vec2[1]]
+}
+
+fn vec_rem(vec1: [isize; 2], vec2: [isize; 2]) -> [isize; 2] {
+	[-vec1[0] + vec2[0], -vec1[1] + vec2[1]]
+}
+
+fn vec_len(vec: [isize; 2]) -> f64 {
+	f64::sqrt((isize::pow(vec[0], 2) + isize::pow(vec[1], 2)) as f64)
+}
+
+// Trims a vector to a max length 1 on both x and y
+fn vec_trim(vec: [isize; 2]) -> [isize; 2] {
+	let mut new_vec = [vec[0], vec[1]];
+	if new_vec[0] != 0 { new_vec[0] = vec[0] / vec[0].abs(); }
+	if new_vec[1] != 0 { new_vec[1] = vec[1] / vec[1].abs() }
+	new_vec
+}
